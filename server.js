1bbb2c0bb6ffb73f0eb2734358160dc910975fc7
Belo001 <thabojohnson882@gmail.com>
2025-12-04 12:35:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ const allowedOrigins = [
   'http://localhost:5174', 
   'http://localhost:5173', 
   'http://127.0.0.1:5174',  
-  'http://127.0.0.1:5173'
+  '127.0.0.1:5173',
+  'https://your-react-app.railway.app' // Add your React app URL here
 ];
 
 // Add Railway domains if they exist
